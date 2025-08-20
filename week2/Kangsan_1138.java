88c6812036bc08a9b0cc2627bf91e29587c8df41
DEV_KANGSAN <devstar1223@gmail.com>
2025-08-20 11:12:52 +0900

    김강산 2주차 풀이

---
+++ week2/Kangsan_1138.java
@@ -0,0 +1,38 @@
+package week2;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+// 한 줄로 서기 (https://www.acmicpc.net/problem/1138)
+public class Kangsan_1138 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // init
+        int N = Integer.parseInt(br.readLine());
+        String[] line = br.readLine().split(" ");
+        int[] array = new int[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(line[i]);
+        }
+        List<Integer> answerList = new ArrayList<>();
+
+        //solve
+        for(int i = N-1; i >= 0; i--){
+            answerList.add(array[i],i+1);
+        }
+
+        //out
+        for(int i = 0; i < N; i++){
+            bw.write(answerList.get(i)+"");
+            if(i != N-1){
+                bw.write(" ");
+            }
+        }
+        bw.flush();
+    }
+}
+
